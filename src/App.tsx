538a789c042b7c8a633b432b55937f239388a48e
Henrique <henrique.scastro12@gmail.com>
2025-03-23 02:52:24 -0300

    Página de Cadastro e Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import RegisterPage from "./pages/Register";
+import LoginPage from "./pages/Login";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    // Minhas rotas
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
